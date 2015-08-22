eb578d8c61353222537dda7fe7cd921344d679c1
Rutger Farry <rutgerfarry@gmail.com>
2015-08-22 00:25:31 -0700

    Initial search works, broken afterwards
    Something about the router is preventing the nabber from performing two
    successful searches. Successive searches still change the URL, but
    don’t actually pull up new results.
    
    Try checking the props.params. Maybe a manual re-render is needed since
    the state isn’t changing.

--- js/components/Navbar.js
+++ js/components/Navbar.js
@@ -1,14 +1,31 @@
 import React from 'react';
-import { Link } from 'react-router';
+import Router, { Link } from 'react-router';
 
 const ClassmereSearchInput = React.createClass({
+  mixins: [Router.Navigation],
+  getInitialState: function searchPageInitialState() {
+    return {text: ''};
+  },
   render: function renderSearch() {
     return (
       <input
         type="text"
         className="form-control"
-        placeholder="search classes..."/>
+        placeholder="search classes..."
+        value={this.state.text}
+        onChange={this.handleOnChange}
+        onKeyDown={this.handleKeyDown}
+        />
     );
+  },
+  handleOnChange: function handleKeyboardInput(event) {
+    this.setState({ text: event.target.value });
+  },
+  handleKeyDown: function handleEnterPressed(event) {
+    if (event.keyCode === 13) {
+      this.transitionTo('search', {searchTerm: this.state.text});
+      this.setState({ text: '' });
+    }
   },
 });
 
