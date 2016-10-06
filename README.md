8380b4a6ae604c5c977be1a1eb94a13f3b542c06
Rutger Farry <rutgerfarry@gmail.com>
2016-10-06 12:42:20 -0700

    Cleared out the dusty closet

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # web
-React web app that serves as the interface for classmere
+web app that serves as the interface for classmere
--- js/pages/Home.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export const HomePage = React.createClass({
-  render: function renderHomePage() {
-    return (
-      <h1 className="text-center display-3">Welcome to classmere</h1>
-    );
-  },
-});
