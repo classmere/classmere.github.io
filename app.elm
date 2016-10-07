5eb66ecac49cc15f2e43af32ca2f99a735d34404
Rutger Farry <rutgerfarry@gmail.com>
2016-10-06 18:08:25 -0700

    Add quick type definition for courses and sections

--- app.elm
+++ app.elm
@@ -78,3 +78,37 @@ searchCourse searchText =
   in
     Task.perform SearchFail SearchSucceed (Http.getString url)
 
+
+-- JSON
+
+type alias Course =
+  { id : String
+  , title : String
+  , subjectCode : String
+  , courseNumber : Int
+  , credits : String
+  , description : String
+  , prereqs : String
+  , sections : List Section
+  }
+
+type alias Section =
+  { term : String
+  , session : String
+  , crn : Int
+  , credits : Int
+  , meetingTimes : String
+  , startDate : String
+  , endDate : String
+  , campus : String
+  , kind : String
+  , status : String
+  , enrollmentCapacity : Int
+  , enrollmentCurrent : Int
+  , waitlistCapacity :Int
+  , waitlistCurrent : Int
+  , fees : String
+  , restrictions : String
+  , comments : String
+  , textbookUrl : String
+  }
