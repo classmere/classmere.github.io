67024955268e4f1dfc4ff47f27528effbae2ed04
Rutger Farry <rutgerfarry@gmail.com>
2016-10-07 17:47:32 -0700

    Working on routing between pages
    Can navigate to course link but not rendering page yet

--- src/App.elm
+++ src/App.elm
@@ -1,21 +1,27 @@
 import Html exposing (..)
-import Html.App as Html
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 import Http
+import Navigation
+import String
 import Task
+import UrlParser exposing (..)
+
 import ClassmereData.Models exposing (Course, Section)
 import ClassmereData.Decoders exposing (courseDecoder)
 
 
+
 main : Program Never
 main =
-  Html.program
+  Navigation.program (Navigation.makeParser stringParser)
     { init = init
     , view = view
     , update = update
+    , urlUpdate = urlUpdate
     , subscriptions = subscriptions
     }
+
 
 
 -- MODEL
@@ -23,7 +29,10 @@ main =
 type alias Model =
   { searchText : String
   , results : List Course
+  , page : Page
   }
+
+
 
 
 -- UPDATE
@@ -37,13 +46,47 @@ update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
     Search searchText ->
-      (Model searchText [], searchCourse searchText)
+      (Model searchText [] model.page, searchCourse searchText)
 
     SearchSucceed resultJson ->
-      (Model model.searchText resultJson, Cmd.none)
+      (Model model.searchText resultJson model.page, Cmd.none)
 
     SearchFail httpError ->
       (model, Cmd.none)
+
+
+
+-- PARSER
+
+toUrl : Page -> String
+toUrl page =
+  case page of
+    Home -> "/"
+    ShowCourse subjectCode courseNumber -> 
+      subjectCode ++ "/" ++ (toString courseNumber)
+
+stringParser : Navigation.Location -> Result String Page
+stringParser location =
+  UrlParser.parse identity desiredPage (String.dropLeft 1 location.hash)
+
+type Page = Home | ShowCourse String Int
+
+desiredPage : Parser (Page -> a) a
+desiredPage =
+  oneOf
+    [ format ShowCourse (UrlParser.s "course" </> string </> int)
+    , format Home (UrlParser.s "")
+    ]
+
+urlUpdate : Result String Page -> Model -> (Model, Cmd Msg)
+urlUpdate result model =
+  case result of
+    Ok page ->
+      ({ model | page = page }, Cmd.none)
+
+    Err _ ->
+      (model, Navigation.modifyUrl (toUrl model.page))
+
 
 
 -- VIEW
@@ -58,9 +101,10 @@ view model =
 renderCourse : Course -> Html Msg
 renderCourse course =
   tr [] 
-    [ td [] [ text course.title ] 
+    [ td [] [ a [ href (toUrl (ShowCourse course.subjectCode course.courseNumber)) ] [ text course.title ] ]
     , td [] [ text course.description ]
     ]
+
 
 
 -- SUBSCRIPTIONS
@@ -70,11 +114,13 @@ subscriptions model =
   Sub.none
 
 
+
 -- INIT
 
-init : (Model, Cmd Msg)
-init =
-  (Model "" [], Cmd.none)
+init : Result String Page -> (Model, Cmd Msg)
+init result =
+  urlUpdate result (Model "" [] Home)
+
 
 
 -- HTTP
@@ -85,4 +131,5 @@ searchCourse searchText =
     url =
       "http://api.classmere.com/search/courses/" ++ searchText
   in
-    Task.perform SearchFail SearchSucceed (Http.get courseDecoder url)
+    Http.get courseDecoder url
+      |> Task.perform SearchFail SearchSucceed
